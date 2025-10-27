44d4f60ec20b15a69707f0ee5007ec83b24f4cf2
yj8431 <yj8431@pleasantonusd.net>
2025-10-27 20:51:17 +0000

    working on book.java

--- piglatin/Book.java
+++ piglatin/Book.java
@@ -85,5 +85,16 @@ public class Book {
     void writeToFile(String name) {
         // TODO: Add code here to write the contents of the book to a file.
         // Must write to file using provided name.
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(title+".txt", true)))
+        {
+            for (int i=0; i<getLineCount();i++)
+            {
+                writer.write(text.get(i));
+                writer.newLine();
+            }
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
     }
+    
 }
