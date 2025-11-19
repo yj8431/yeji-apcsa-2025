a8fe199a686d59b6126be70c20a3c04cdb4c3f24
yj8431 <yj8431@pleasantonusd.net>
2025-11-19 02:33:44 +0000

    worked on deck class today, set up deck constructor making deck of cards

--- cards/Deck.java
+++ cards/Deck.java
@@ -1,6 +1,53 @@
 package cards;
+import java.util.Random;
 
 public class Deck {
 
+    private int top;
+    Card[] deck = new Card[52];
 
+    public Deck()
+    {
+        //Card[] deck = new Card[52];
+        int top = 0;
+        for (int i=0; i<13; i++)
+        {
+            deck[i] = new Card(0,i);
+        }
+        for (int i=13; i<26; i++)
+        {
+            deck[i] = new Card(1,i);
+        }
+        for (int i=26; i<39; i++)
+        {
+            deck[i] = new Card(2,i);
+        }
+        for (int i=39; i<52; i++)
+        {
+            deck[i] = new Card(3,i);
+        }
+    }
+
+    public void shuffle()
+    {
+        //randomizes order of deck
+        for (int i=deck.length; i>0; i--)
+        {
+            int randnum = (int) (Math.random()*(i+1));
+            deck[]
+        }
+
+    }
+
+    public void cut(int position)
+    {
+
+    }
+
+    public String draw()
+    {
+
+    }
+
+    public void print(int )
 }
