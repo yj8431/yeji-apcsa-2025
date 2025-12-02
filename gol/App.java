34ba7574585600e751b79ccf2a1b26fbdf34a97c
Mengjiao Cyr <mcyr@pleasantonusd.net>
2025-12-01 19:48:42 -0800

    Updated project files.

---
+++ gol/App.java
@@ -0,0 +1,21 @@
+package gol;
+
+public class App {
+
+    public static void main(String[] args)
+    {
+        // GameOfLife g = new GameOfLife(7, 7);
+
+        // int[][] data = {
+        //     { 0, 1, 0},
+        //     { 0, 0, 1},
+        //     { 1, 1, 1 }
+        // };
+
+        // g.set(1, 1, data);
+
+        // g.run(5);
+
+        TestSuite.run();
+    }
+}
