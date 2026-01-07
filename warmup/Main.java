34dedb2c739b96a63844bcefb2050abd00662ef7
yj8431 <yj8431@pleasantonusd.net>
2026-01-07 22:37:27 +0000

    pulled files, set up warm up project

--- warmup/Main.java
+++ warmup/Main.java
@@ -1,9 +1,18 @@
 package warmup;
 
 public class Main {
+    //problem 1
 
     public static void main(String[] args) {
         // Your code here.
+        int sum = 0;
+        for (int i=1; i<1000; i++)
+        {
+            if (i%3==0 || i%5==0)
+            {
+                sum+=i;
+            }
+        }
 
     }
 }
