ab55a05d55335ed9a99e97834456cc47eceb8b54
yj8431 <yj8431@pleasantonusd.net>
2026-01-27 22:12:50 +0000

    worked on selection and insertion sort

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -4,5 +4,22 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
+        int[] newList = new int[input.length];
+        for (int i=0; i<input.length; i++)
+        {
+            if (input[i]<input[i+1])
+            {
+                newList[i] = input[i];
+            }
+            else
+            {
+                //compare with previous terms
+                for (int a=i+1; a>0; a--)
+                {
+                    if (input[a])
+                }
+            }
+        }
+
     }
 }
--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -4,6 +4,30 @@ public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
+        int smallest=input[0];
+        int smallestPosition = 0;
+        for (int i=0; i<input.length; i++)
+        {
+            for (int a=i; a<input.length; a++)
+            {
+                if (input[a]<input[a+1])
+                {
+                    smallest = input[a];
+                    smallestPosition = a;
+                }
+                else
+                {
+                    smallest = input[a+1];
+                    smallestPosition = a+1;
+                }
+            }
+            int temp = input[i];
+            input[i] = smallest;
+            input[smallestPosition] = temp;
+            
+
+        }
+        System.out.println(input);
     }
     
 }
