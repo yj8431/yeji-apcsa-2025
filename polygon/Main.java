1024167bb095f9b02d6f68854ac35cc4e6e02d61
Mengjiao Cyr <mcyr@pleasantonusd.net>
2026-02-21 19:26:42 -0800

    Updated project files.

---
+++ polygon/Main.java
@@ -0,0 +1,12 @@
+package polygon;
+
+public class Main {
+    public static void main(String [] args)
+    {
+        IrregularPolygon myPolygon = new IrregularPolygon();
+        myPolygon.draw();
+
+        TestSuite.run();
+    }
+    
+}
