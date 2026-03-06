3e6c1dd21b02c7ba13da7dc462e9630b30d6a41d
yj8431 <yj8431@pleasantonusd.net>
2026-03-06 21:57:07 +0000

    passed test suite

--- polygon/Main.java
+++ polygon/Main.java
@@ -7,24 +7,26 @@ public class Main {
     {
         IrregularPolygon myPolygon = new IrregularPolygon();
         //testing points for drawing a square
-        /*
+        
         myPolygon.add(new Point2D.Double(0,0));
         myPolygon.add(new Point2D.Double(0,100));
         myPolygon.add(new Point2D.Double(100,100));
         myPolygon.add(new Point2D.Double(100,0));
         
         myPolygon.draw();
-        */
+        
 
         //test point parallelogram draw
+        /*
         double[][] parallelogramPoints = {{20,10},{70,20},{50,50},{0,40}};
         for (double[] p : parallelogramPoints)
         {
             Point2D.Double point = new Point2D.Double(p[0],p[1]);
             myPolygon.add(point);
         }
+        */
         
-        myPolygon.draw();
+        //myPolygon.draw();
 
         TestSuite.run();
     }
