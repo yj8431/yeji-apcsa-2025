8c04460dca7228ac5ad196b7b336934d753d66ec
Mengjiao Cyr <mcyr@pleasantonusd.net>
2026-03-15 20:15:59 -0700

    Updated project files.

---
+++ tower/Main.java
@@ -0,0 +1,17 @@
+package tower;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Example: Solve a tower
+        // TowerModel model = new TowerModel(3);
+        //TowerSolver solver = new TowerSolver();
+        //solver.solve(model);
+
+        // Run test suite
+        TestSuite test = new TestSuite();
+        test.run();
+
+    }
+}
---
+++ tower/TowerSolver.java
@@ -0,0 +1,23 @@
+package tower;
+
+public class TowerSolver {
+    private TowerModel model;
+
+    public TowerSolver()
+    {
+        // Nothing to do here
+    }
+
+    public void solve(TowerModel model)
+    {
+        this.model = model;
+        // Call the missing solve method (not this one)
+    }
+
+    // Create an overloaded solve(...) method
+    // This new method will be recursive (call itself)
+    //
+    // [ solve method here]
+    //
+
+}
